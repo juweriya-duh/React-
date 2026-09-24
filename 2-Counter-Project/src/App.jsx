2f20a0e64ef770b14fafa8356479f74ff2eb1a41
Juweriya Fatima <jijee1013@gmail.com>
2026-09-24 14:09:18 +0530

    done with Counter Project

--- 2-Counter-Project/src/App.jsx
+++ 2-Counter-Project/src/App.jsx
@@ -1,121 +1,49 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [counter, setCounter]= useState(7)
+
+  //let counter = 7
+  const addValue = () => {
+  //  counter = counter + 1
+  //   console.log(counter);
+
+  /*Batching: all the values will be updated just once as a batch.
+  if we want to fix this we will have to use a new arrow func 
+  inside the setCounter() method
+
+  setCounter(counter+1)
+  setCounter(counter+1)
+  setCounter(counter+1)
+  setCounter(counter+1)
+  */
+
+ setCounter((prevCounterrrrr) => prevCounterrrrr +1)
+ setCounter((prevCounterrrrr) => prevCounterrrrr +1)
+ setCounter((prevCounterrrrr) => prevCounterrrrr +1)
+ setCounter((prevCounterrrrr) => prevCounterrrrr +1)
+
+
+  //these cant be baatched because they are dependent on the prev values.
+  }
+
+  const removeValue = () => {
+    setCounter(counter-1)
+  }
+
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <h1>React Course With Juwe {counter}</h1>
+    <h2>Counter value: {counter}</h2>
+    <button onClick={addValue}>Add Value</button> {"   "}
+    <button onClick={removeValue}>Remove Value</button>
+    <p>footer: {counter}</p>
+     </>
   )
 }
 
