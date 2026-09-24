3ab7b5f416be9d805fe41809f4104c071f5af9d1
Juweriya Fatima <jijee1013@gmail.com>
2026-09-24 13:29:45 +0530

    made CustomReact in Vite

--- 1-vite/src/App.jsx
+++ 1-vite/src/App.jsx
@@ -1,17 +1,24 @@
 
 import Youtube from "./Youtube"
 
+
+
+const username = "juweriya"
+
 function App() {
  
-
   return (
     <>
-    <h1>Vite react app</h1>
-    <h1>Vite react app</h1>
+    <h1>Vite react app  {2+5}</h1>
+    <h1>Vite react app {username}</h1>
+
     <Youtube />
+  
     
   </>
   )
 }
+
+
 
 export default App
--- 1-vite/src/main.jsx
+++ 1-vite/src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import React from 'react'
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank_baby'
+    },
+    children: 'click me to visit google'
+}
+
+function MyCustomReact(){  //pascal case
+return(
+  <div>
+      <h1>    Custom React App from scratch
+</h1>
+  </div>
+)
+}
+
+const AnotherElement = (
+  <a href="http://google.com" target='_blank_baby'>
+    visit google
+  </a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: "_blank_baby"},
+  'click to visit gooooogle'
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  areactElement
+
 )
